f167855c92d371fe4a9d5f32e4dd34085efaabaa
Maksim <kretovmk@gmail.com>
2017-10-03 02:14:34 +0300

    need to check shapes of tensors

--- mpnn/mpnn.py
+++ mpnn/mpnn.py
@@ -1,27 +1,61 @@
 
 import deepchem as dc
-from rdkit import Chem
-
+import torch.optim as optim
+import copy
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 
+from rdkit import Chem
 from torch.autograd import Variable
 from collections import OrderedDict
 
-
+# Some parts of code taken from https://github.com/deepchem/deepchem/blob/master/contrib/mpnn/mpnn.py
+# MIT License
 
 class MPNN:
     """
     Performs full message passing procedure.
     """
+    def __init__(self, M_start, M_hid, U, R, t):
+        assert t > 0, 'ERROR: incorrect T value.'
+        self.params = []
+        self._construct_message_passing_func(M_start, M_hid, U, R, t)
+        self._add_params()
+        self.opt = optim.Adam(self.params, lr=1e-3)
 
-    def __init__(self, ):
-        self.params = None
+    def make_opt_step(self, batch, t):
+        x, y = batch
+        self.opt.zero_grad()
+        loss = Variable(torch.zeros(1, 1))
+        for i in range(len(x)):
+            smile = x[i]
+            y_true = y[i]
+            g, h = self.get_features_from_smiles(smile)
+            for k in range(0, t):
+                self.single_message_pass(g, h, k)
+            y_pred = self.R(h)
+            loss += (y_pred - y_true) **2 / Variable(torch.FloatTensor([len(x)])).view(1, 1)
+        print(loss)
+        loss.backward()
+        self.opt.step()
 
-    def add_params(self, params):
-        self.params.append(params)
+    def _construct_message_passing_func(self, M_start, M_hid, U, R, t):
+        self.R = R
+        self.U, self.M = {}, {}
+        self.M[0] = M_start
+        self.U[0] = copy.deepcopy(U)
+        for i in range(1, t):
+            self.M[i] = copy.deepcopy(M_hid)
+            self.U[i] = copy.deepcopy(U)
+
+    def _add_params(self):
+        self.params += list(self.R.parameters())
+        for k, v in self.M.items():
+            self.params += list(v.parameters())
+        for k, v in self.U.items():
+            self.params += list(v.parameters())
 
     def get_features_from_smiles(self, smile):
         g = OrderedDict({})  # edges
@@ -41,17 +75,21 @@ class MPNN:
                         g[i].append((e_ij, j))
         return g, h
 
-
-    def message_pass(self, g, h, k):
+    def single_message_pass(self, g, h, k):
+        h_before = {}
+        for key, value in h.items():
+            h_before[key] = Variable(copy.deepcopy(value.data))
         for v in g.keys():
             neighbors = g[v]
+            m_v_k = None
             for neighbor in neighbors:
                 e_vw = neighbor[0]  # bond feature (between v and w)
                 w = neighbor[1]  # atom w feature
-                m_w = V[k](h[w])
-                m_e_vw = E(e_vw)
-                reshaped = torch.cat((h[v], m_w, m_e_vw), 1)
-                h[v] = F.selu(U[k](reshaped))
+                if m_v_k is None:
+                    m_v_k = self.M[k](torch.cat((h_before[v], h_before[w], e_vw), dim=1))
+                else:
+                    m_v_k += self.M[k](torch.cat((h_before[v], h_before[w], e_vw), dim=1))
+            h[v] = self.U[k](torch.cat((h_before[v], m_v_k), dim=1))
 
 
 
