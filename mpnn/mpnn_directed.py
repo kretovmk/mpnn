9c6c5db15a311ee532abfb721a3a17855090044b
Maksim Kretov <kretovmk@gmail.com>
2017-10-26 22:31:40 +0300

    video batch

--- mpnn/mpnn_directed.py
+++ mpnn/mpnn_directed.py
@@ -8,6 +8,7 @@ import numpy as np
 
 from torchfold import Fold
 from rdkit import Chem
+from utils import CUDA_wrapper
 from torch.autograd import Variable
 from collections import OrderedDict
 
@@ -58,8 +59,8 @@ class MPNNdirected:
             self.E.cuda()
         for i in range(t):
             if self.cuda:
-                setattr(self, 'V_{}'.format(i), copy.deepcopy(V.cuda()))
-                setattr(self, 'U_{}'.format(i), copy.deepcopy(U.cuda()))
+                setattr(self, 'V_{}'.format(i), copy.deepcopy(V).cuda())
+                setattr(self, 'U_{}'.format(i), copy.deepcopy(U).cuda())
             else:
                 setattr(self, 'V_{}'.format(i), copy.deepcopy(V))
                 setattr(self, 'U_{}'.format(i), copy.deepcopy(U))
@@ -78,17 +79,13 @@ class MPNNdirected:
         for i in range(0, molecule.GetNumAtoms()):
             atom_i = molecule.GetAtomWithIdx(i)
             features = dc.feat.graph_features.atom_features(atom_i).astype(np.float32)
-            atom_var = Variable(torch.FloatTensor(features)).view(1, 75)
-            if cuda:
-                atom_var.cuda()
+            atom_var = Variable(CUDA_wrapper(torch.FloatTensor(features), cuda).view(1, 75))
             h[i] = atom_var
             for j in range(0, molecule.GetNumAtoms()):
                 e_ij = molecule.GetBondBetweenAtoms(i, j)
                 if e_ij != None:
                     e_ij = list(map(lambda x: 1 if x == True else 0, dc.feat.graph_features.bond_features(e_ij)))
-                    edge_var = Variable(torch.FloatTensor(e_ij).view(1, 6))
-                    if cuda:
-                        edge_var.cuda()
+                    edge_var = Variable(CUDA_wrapper(torch.FloatTensor(e_ij), cuda).view(1, 6))
                     e_ij = edge_var
                     if i not in g:
                         g[i] = []
@@ -106,9 +103,6 @@ class MPNNdirected:
                 reshaped = torch.cat((h[v], m_w, m_e_vw), 1)
                 h[v] = F.tanh(getattr(self, 'U_{}'.format(k))(reshaped))
 
-    # def fold_cat(self, x1, x2, x3):
-    #     return torch.cat((x1, x2, x3), 1)
-
     def fold_non_lin(self, x):
         return F.tanh(x)
 
@@ -120,72 +114,27 @@ class MPNNdirected:
                 w = neighbor[1]  # atom w number
                 m_w = fold.add('V_{}'.format(k), h[w])
                 m_e_vw = fold.add('E', e_vw)
-                #reshaped = fold.add('fold_cat', h[v], m_w, m_e_vw)
-                #res = fold.add('U_{}'.format(k), reshaped)
-                #reshaped = fold.add('fold_cat', h[v], m_w, m_e_vw)
                 res = fold.add('U_{}'.format(k), h[v], m_w, m_e_vw)
                 h[v] = fold.add('fold_non_lin', res)
-        # return fold, [list(h.values())]
-        # result = fold.apply(self, [list(h.values())])[0].split(1, dim=0)
-        # for i, r in enumerate(result):
-        #     h[i] = r
 
-
-    def make_opt_step_batched(self, batch_x, batch_y, t):
-        #print('*')
+    # TODO: delete repetitions
+    def make_opt_step_batched(self, smiles, batch_x, batch_x2, batch_y, t):
         self.opt.zero_grad()
         loss = Variable(torch.zeros(1, 1))
         fold = Fold()
         g_dict, h_dict, h2_dict, y_true_dict = OrderedDict(), OrderedDict(), OrderedDict(), OrderedDict()
-        # for i in range(len(batch_x)):
-        #     smile = batch_x[i]
-        #     y_true_dict[smile] = batch_y[i]
-        #
-        #     g_dict[smile], h_dict[smile] = self.get_features_from_smiles(smile)
-        #     _, h2_dict[smile] = self.get_features_from_smiles(smile)
-        #
-        #     for k in range(0, t):
-        #         self.single_message_pass_dyn_batched(g_dict[smile], h_dict[smile], k, fold)
-
         for i in range(len(batch_x)):
-            smile = batch_x[i]
-            y_true = batch_y[i]
-
-            g, h = self.get_features_from_smiles(smile)
-            _, h2 = self.get_features_from_smiles(smile)
-
-            for k in range(0, t):
-                self.single_message_pass_dyn_batched(g, h, k, fold)
-                #print(k)
-                #print('************')
-                #print(fold.cached_nodes['fold_cat'].keys())
-
-
-
-        # folded_nodes = h
-        # result = fold.apply()
-        self.fold = fold
-        self.h = h
-        #folded_nodes = []
-        #for k, v in h.items():
-        #    folded_nodes.append(list(v.values()))
-
-        #print(folded_nodes)
-        #result = fold.apply(self, folded_nodes)
-        #print(len(fold.cached_nodes))
-        result = fold.apply(self, [list(h.values())])
-        #print('1111')
-
-
-
-
-        #     y_pred = self.R(h_dict[smile], h2_dict[smile])
-        #     loss += (y_pred - y_true) * (y_pred - y_true) / Variable(torch.FloatTensor([len(batch_x)])).view(1, 1)
-        # loss.backward()
-        # self.opt.step()
-        # return loss.data[0][0]
-
-
-
-
-
+            print(i)
+            smile = smiles[i]
+            g_dict[smile], h_dict[smile] = batch_x[i]
+            _, h2_dict[smile] = batch_x2[i]
+            y_true_dict[smile] = batch_y[i]
+            for k in range(t):
+                self.single_message_pass_dyn_batched(g_dict[smile], h_dict[smile], k, fold)
+        folded_nodes = []
+        for k, v in h_dict.items():
+            print(k)
+            print(len(v))
+            folded_nodes.append(list(v.values()))
+        results = fold.apply(self, folded_nodes)
+        return results, folded_nodes
--- utils.py
+++ utils.py
@@ -1,6 +1,11 @@
 
 import numpy as np
 
+def CUDA_wrapper(tensor, cuda):
+    if cuda:
+        return tensor.cuda()
+    else:
+        return tensor
 
 def rolling_mean(arr, n):
     """Calculate rolling mean."""
